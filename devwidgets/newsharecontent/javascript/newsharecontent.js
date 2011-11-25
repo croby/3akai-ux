cb0965673eee333c88552331ff33992be1d342b6
Chris Roby <christopher.roby@gmail.com>
2011-11-25 15:31:18 -0800

    SAKIII-4137 Adding framework to newsharecontent

--- devwidgets/newsharecontent/javascript/newsharecontent.js
+++ devwidgets/newsharecontent/javascript/newsharecontent.js
@@ -233,7 +233,10 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai){
             $.ajaxSettings.cache = true;
             $.getScript('//s7.addthis.com/js/250/addthis_widget.js?%23pubid=' + sakai.widgets.newsharecontent.defaultConfiguration.newsharecontent.addThisAccountId + '&domready=1');
             $.ajaxSettings.cache = ajaxcache;
-            sakai.api.Util.AutoSuggest.setup($newsharecontentSharelist, {"asHtmlID": tuid});
+            sakai.api.Util.AutoSuggest.setup( $newsharecontentSharelist, {
+                asHtmlID: tuid,
+                scrollHeight: 120
+            });
             $("label#newsharecontent_autosuggest_for").attr("for", tuid);
         };
 
